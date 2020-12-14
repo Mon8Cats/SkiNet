e2f119cd760f0b73a80be05b8ad2c0c078209939
StoneRoot <stoneroot@StoneRoots-MBP.lan>
2020-12-14 14:18:29 -0600

    (8) Add Pagination

---
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -0,0 +1,18 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/shared/models/product';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss']
+})
+export class ProductItemComponent implements OnInit {
+  @Input() product?: IProduct;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
